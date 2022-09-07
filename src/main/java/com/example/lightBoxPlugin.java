7cb7b6eb5354672da74c4963a4b2b91da031adae
dennieb <dennieford@hotmail.com>
2022-09-07 11:30:44 +0300

    - Changed project name to lightBox.

--- src/main/java/com/example/lightBoxPlugin.java
+++ src/main/java/com/example/lightBoxPlugin.java
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class lightBoxPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private lightBoxConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    lightBoxConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(lightBoxConfig.class);
 	}
 }
--- src/test/java/com/example/lightBoxPluginTest.java
+++ src/test/java/com/example/lightBoxPluginTest.java
@@ -3,11 +3,11 @@ package com.example;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class lightBoxPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(lightBoxPlugin.class);
 		RuneLite.main(args);
 	}
 }
